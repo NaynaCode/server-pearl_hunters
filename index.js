6808289d1f7da09c51fada6722f9fb373c12b09a
NaynaCode <nadjamarkicevic@gmail.com>
2024-08-12 21:09:50 +0200

    server update

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const UserModel = require('./models/Users')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Users');
 
 const app = express();
 app.use(express.json());
 
+const corsOptions = {
+    origin: 'https://pearl-hunters-client.vercel.app',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+};
 
-app.use(cors());
-
-
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 const port = 3000;
 
 mongoose.connect("mongodb+srv://nadja:DojNDGDGsajuGrca@pearl-hunters.qeuam.mongodb.net/pearl-hunters?retryWrites=true&w=majority&appName=pearl-hunters");
 
-app.get("/getUsers", (req, res) => {
-    UserModel.find({})
-    .then(function(users) {
-        console.log(users);
-        res.json(users)
-    }).catch(function(err) {
-        res.json(err)
-    })
-})
-
-app.post('/api/players', (req, res) => {
-    console.log('Received a POST request to /api/players'); // Debug log
-    const { username } = req.body;
-    
-    // Check if the username is provided
-    if (!username) {
-        console.error('No username provided'); // Debug log
-        return res.status(400).json({ error: 'Username is required' });
+app.get("/api/players", async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-    
-    console.log(`Player logged in: ${username}`); // Log player login
-    // Send a welcome message back to the client
-    res.json({ message: `Welcome, ${username}!` });
+});
+
+app.post("/api/players", async (req, res) => {
+    try {
+        const user = req.body;
+        const newUser = new UserModel(user);
+        await newUser.save();
+        res.json({ message: `Welcome, ${user.username}!` });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}...`);
-})
+});
