3dd440272466f39c063f45577fd761bbded2ee69
NaynaCode <nadjamarkicevic@gmail.com>
2024-08-16 11:05:07 +0200

    server update

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: 'https://pearl-hunters-client.vercel.app', // No trailing slash
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Add this line if you're using cookies or need to pass credentials
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 const port = 3000;
 
