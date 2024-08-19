fd174309d606450951fbba912861ed685cf0e3e6
NaynaCode <nadjamarkicevic@gmail.com>
2024-08-19 11:29:35 +0200

    server works!

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8080", // Allow requests from this origin
+        origin: "https://pearl-hunters-client.vercel.app", // Allow requests from this origin
         methods: ["GET", "POST"], // Specify which methods are allowed
     }
 });
@@ -17,7 +17,7 @@ const io = new Server(server, {
 app.use(express.json());
 const corsOptions = {
     origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:8080'];
+        const allowedOrigins = ['https://pearl-hunters-client.vercel.app'];
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
         } else {
